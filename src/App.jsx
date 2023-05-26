d901c46e97338dfbb8fe33de4a13001ad0cd02b9
daoanhhuy26012001 <daoanhhuy26012001@gmail.com>
2023-05-26 17:00:10 +0700

    #2 Render registration form and do validation by pattern and add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,91 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './app.css'
+import FormInput from './components/Input'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    birthday: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const inputs = [
+    {
+      id: 1,
+      name: 'username',
+      type: 'text',
+      placeholder: 'Username',
+      errorMessage:
+        'Username should be 3-16 characters and should not include any special character!',
+      label: 'Username',
+      pattern: '^[A-Za-z0-9]{3,16}$',
+      required: true
+    },
+    {
+      id: 2,
+      name: 'email',
+      type: 'email',
+      placeholder: 'Email',
+      errorMessage: 'It should be a valid email address!',
+      label: 'Email',
+      required: true
+    },
+    {
+      id: 3,
+      name: 'birthday',
+      type: 'date',
+      placeholder: 'Birthday',
+      label: 'Birthday'
+    },
+    {
+      id: 4,
+      name: 'password',
+      type: 'password',
+      placeholder: 'Password',
+      errorMessage:
+        'Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!',
+      label: 'Password',
+      pattern:
+        '^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$',
+      required: true
+    },
+    {
+      id: 5,
+      name: 'confirmPassword',
+      type: 'password',
+      placeholder: 'Confirm Password',
+      errorMessage: 'Passwords do not match!',
+      label: 'Confirm Password',
+      pattern: values.password,
+      required: true
+    }
+  ]
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
